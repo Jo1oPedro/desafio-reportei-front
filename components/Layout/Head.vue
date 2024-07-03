a0a993d619f52312f50406f3ecf6b68cd127622e
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-03 14:30:40 -0300

    Adiciona logou ao header

--- components/Layout/Head.vue
+++ components/Layout/Head.vue
@@ -8,23 +8,34 @@
       </Button>
     </div>
 
-    <ColorModeToogle></ColorModeToogle>
+    <div class="flex">
+      <Button @click="logout()" class="bg-transparent hover:bg-gray-400">
+        <Icon name="majesticons:logout" size="20" :color="iconColor"></Icon>
+      </Button>
+      <ColorModeToogle></ColorModeToogle>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useAuthStore } from "@/stores/auth";
+
 defineProps(["eyeIcon"]);
-const emit = defineEmits(["toggleSidebar"]);
 
 const colorMode = useColorMode();
-
 const iconColor = ref(colorMode.value === "dark" ? "white" : "black");
 watch(colorMode, function () {
   iconColor.value = colorMode.value === "dark" ? "white" : "black";
 });
 
+const emit = defineEmits(["toggleSidebar"]);
 function toggleSidebar() {
   emit("toggleSidebar");
+}
+
+const { logUserOut } = useAuthStore();
+function logout() {
+  logUserOut();
 }
 </script>
 
