e2872302e84ca593a35d13f81bf326ae93a4ab97
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-04 14:09:32 -0300

    Adiciona erro e altera nome do método para consultar os repositorios

--- stores/github.ts
+++ stores/github.ts
@@ -2,21 +2,31 @@ import { defineStore } from "pinia";
 
 export const useGithubStore = defineStore({
   id: "myGithubStore",
+  state: () => ({
+    loading: false,
+    error: "",
+  }),
   actions: {
-    async getAuthenticatedUser() {
-        const token = useCookie("token");
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token.value}`,
-                "Content-Type": "application/json",
-            },
+    async getAllUserRepositories() {
+      const token = useCookie("token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token.value}`,
+          "Content-Type": "application/json",
+        },
+      };
+      const { data, error } = await useFetch(
+        "http://nginx:8888/api/github/repositories",
+        {
+          headers: {
+            Authorization: `Bearer ${token.value}`,
+          },
         }
-        const {data, error } = await useFetch("http://nginx:8888/api/github/repositories", {
-            headers: {
-              Authorization: `Bearer ${token.value}`,
-            },
-        });
-        //return response.data.id;
+      );
+      if (error.value) {
+        this.error = error.value.data;
+      }
+      return data.value;
     },
   },
 });
