8acdcfa939424aa77d838ae0bbde536f491a8e1b
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-04 15:13:02 -0300

    Altera cor da borda dos repositorios e adiciona badge para a linguagem principal

--- components/Github/Repository.vue
+++ components/Github/Repository.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="w-full">
-    <div class="bg-gray-200 rounded-full w-full p-4">
+    <div class="border-2 border-blue-200 rounded-full w-full p-4">
       <div class="flex gap-3 pb-1">
-        <h1 class="p-0 text-black">{{ repository.full_name }}</h1>
+        <h1 class="p-0">{{ repository.full_name }}</h1>
         <p class="rounded-full bg-gray-300 px-3 text-sm text-black">
           {{ repository.private ? "Private" : "Public" }}
         </p>
       </div>
       <div class="flex items-center gap-3">
         <div class="rounded-full w-2 h-2 bg-green-400"></div>
-        <p>{{ repository.language ?? "X" }}</p>
+        <Badge>{{ repository.language ?? "X" }}</Badge>
         <p class="truncate">{{ repository.description }}</p>
       </div>
     </div>
@@ -17,6 +17,7 @@
 </template>
 
 <script setup lang="ts">
+import { Badge } from "@/components/ui/badge";
 const props = defineProps({
   repository: {
     type: Object,
