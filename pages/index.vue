e0c7eea53a7905f17e87a7ba433a65200b9aa499
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-04 13:49:37 -0300

    Adiciona consulta dos repositorios do usuário e envio das informações para o componente GithubRepository

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="container flex flex-col gap-3">
     <Input type="text" name="repositoryName" placeholder="Find a repository" />
-    <GithubRepository v-for="dale in 3" :key="dale" :id="dale"></GithubRepository>
+    <GithubRepository
+      v-for="repository in repositories"
+      :key="repository.id"
+      :repository="repository"
+    ></GithubRepository>
   </div>
 </template>
 
 <script setup lang="ts">
-import axios from "axios";
 import { useGithubStore } from "@/stores/github";
 
-const { getAuthenticatedUser } = useGithubStore();
-getAuthenticatedUser();
+const { getAllUserRepositories } = useGithubStore();
+const repositories = await getAllUserRepositories();
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
