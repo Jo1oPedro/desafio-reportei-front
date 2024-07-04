f81e41d9c2ffd29b8ce0428e3b48343686d6d921
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-04 15:11:26 -0300

    Altera cor padrão dos icones de alteração de tema

--- components/ColorModeToogle.vue
+++ components/ColorModeToogle.vue
@@ -18,12 +18,12 @@ const colorMode = useColorMode();
         <Button variant="outline" class="bg-transparent border-none">
           <Icon
             icon="radix-icons:sun"
-            color="black"
+            color="white"
             class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
           />
           <Icon
             icon="radix-icons:moon"
-            color="black"
+            color="white"
             class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
           />
           <span class="sr-only">Toggle theme</span>
