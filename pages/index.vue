01c44a606334c6469a632ac43d2abb8ae0096014
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-04 14:31:16 -0300

    Adiciona exibição de erro com componente de alerta

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="container flex flex-col gap-3">
-    <Input type="text" name="repositoryName" placeholder="Find a repository" />
-    <GithubRepository
-      v-for="repository in repositories"
-      :key="repository.id"
-      :repository="repository"
-    ></GithubRepository>
+  <div class="container">
+    <LayoutAlert v-if="error.message" :error="error" />
+    <div class="container flex flex-col gap-3" v-else>
+      <Input
+        type="text"
+        name="repositoryName"
+        placeholder="Find a repository"
+      />
+      <GithubRepository
+        v-for="repository in repositories"
+        :key="repository.id"
+        :repository="repository"
+      ></GithubRepository>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useGithubStore } from "@/stores/github";
+import { storeToRefs } from "pinia";
 
 const { getAllUserRepositories } = useGithubStore();
+const { error } = storeToRefs(useGithubStore());
 const repositories = await getAllUserRepositories();
 </script>
 
