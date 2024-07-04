13563b168bd209d36c178ce9a507d78bfbfca501
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-04 14:27:53 -0300

    Adiciona components de alerta

---
+++ components/Layout/Alert.vue
@@ -0,0 +1,20 @@
+<template>
+  <Alert class="flex items-center justify-center gap-3 flex-col border-red-500">
+    <AlertTitle>{{ error.title }}</AlertTitle>
+    <AlertDescription>
+      {{ error.message }}
+    </AlertDescription>
+  </Alert>
+</template>
+
+<script setup lang="ts">
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+defineProps({
+  error: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style scoped></style>
