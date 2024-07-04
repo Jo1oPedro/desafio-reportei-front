b800651d3a398ea11964ba2ba9b38a974cd8d529
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-04 13:45:36 -0300

    Adiciona informações sobre o repositorio no componente e corrige layout

--- components/Github/Repository.vue
+++ components/Github/Repository.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="flex flex-col gap-0 w-full">
-        <div class="bg-gray-200 rounded-full w-full p-4">
-            <div class="flex gap-3">
-                <h1 class="p-0 text-black">Titulo do repositorio</h1>
-                <p class="rounded-full bg-gray-300 px-3 text-sm text-black">Privado</p>
-            </div>
-            <div class="flex items-center gap-3">
-                <div class="rounded-full w-2 h-2 bg-green-400"></div>
-                <p>Descrição do repositorio</p>
-            </div>
-        </div>
+  <div class="w-full">
+    <div class="bg-gray-200 rounded-full w-full p-4">
+      <div class="flex gap-3 pb-1">
+        <h1 class="p-0 text-black">{{ repository.full_name }}</h1>
+        <p class="rounded-full bg-gray-300 px-3 text-sm text-black">
+          {{ repository.private ? "Private" : "Public" }}
+        </p>
+      </div>
+      <div class="flex items-center gap-3">
+        <div class="rounded-full w-2 h-2 bg-green-400"></div>
+        <p>{{ repository.language ?? "X" }}</p>
+        <p class="truncate">{{ repository.description }}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps({
-  id: {
+  repository: {
+    type: Object,
     required: true,
   },
 });
-
-console.log("O id é: " + props.id);
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
