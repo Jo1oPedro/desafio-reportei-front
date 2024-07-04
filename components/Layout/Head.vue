a32de9d17b41bca3a21948bd938a86cd1dd862e7
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-04 15:12:04 -0300

    Altera cor padrão da sidebar e dos icones

--- components/Layout/Head.vue
+++ components/Layout/Head.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    class="sticky top-0 z-40 bg-gray-300 flex justify-between items-center gap-3 border-b-2"
+    class="sticky top-0 z-40 bg-[#0C1117] flex justify-between items-center gap-3 border-b-2"
   >
     <div>
       <Button @click="toggleSidebar()" class="bg-transparent hover:bg-gray-400">
-        <Icon name="mdi:show" size="20" color="black"></Icon>
+        <Icon name="mdi:show" size="20" color="white"></Icon>
       </Button>
     </div>
 
     <div class="flex">
       <Button @click="logout()" class="bg-transparent hover:bg-gray-400">
-        <Icon name="majesticons:logout" size="20" color="black"></Icon>
+        <Icon name="majesticons:logout" size="20" color="white"></Icon>
       </Button>
       <ColorModeToogle></ColorModeToogle>
     </div>
