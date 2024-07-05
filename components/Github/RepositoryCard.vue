9ec3d361dcacd1ed73e3e21bb60e9b15ea5fb371
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-05 15:35:40 -0300

    Altera url para consultar repositorio especifico

--- components/Github/RepositoryCard.vue
+++ components/Github/RepositoryCard.vue
@@ -1,9 +1,11 @@
 <template>
-  <NuxtLink :to="`/repositories/${repository.id}`">
+  <NuxtLink
+    :to="`/repositories/${repository.owner.login}/${repository.name}/${repository.id}`"
+  >
     <div class="w-full">
       <div class="border-2 border-blue-200 rounded-full w-full p-4">
         <div class="flex gap-3 pb-1">
-          <h1 class="p-0 truncate">{{ repository.full_name }}</h1>
+          <h1 class="p-0 truncate">{{ repository.name }}</h1>
           <p class="rounded-full bg-gray-300 px-3 text-sm text-black">
             {{ repository.private ? "Private" : "Public" }}
           </p>
---
+++ pages/repositories/[owner_name]/[repository_name]/[repository_id].vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!--<LayoutRepository :repository="repository"></LayoutRepository>-->
+    dale123
+  </div>
+</template>
+
+<script setup>
+import { useGithubStore } from "@/stores/github";
+
+const { owner_name, repository_name, repository_id } = useRoute().params;
+
+const { getSpecificUserRepository } = useGithubStore();
+
+const repository = getSpecificUserRepository(
+  owner_name,
+  repository_name,
+  repository_id
+);
+console.log(repository);
+</script>
+
+<style></style>
