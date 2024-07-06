f9cf87c6a5474d1a5d5fdc7e45352c5b81d62e57
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-06 03:37:02 -0300

    Adiciona total de repositorios ao head

--- components/Layout/Head.vue
+++ components/Layout/Head.vue
@@ -1,30 +1,43 @@
 <template>
-  <div
-    class="sticky top-0 z-40 bg-[#0C1117] flex justify-between items-center gap-3 border-b-2"
-  >
-    <div>
-      <Button @click="toggleSidebar()" class="bg-transparent hover:bg-gray-400">
-        <Icon name="mdi:show" size="20" color="white"></Icon>
-      </Button>
-    </div>
+  <div class="sticky top-0 z-40 bg-[#0C1117] flex-col border-b-2">
+    <div class="flex justify-between items-center gap-3">
+      <div>
+        <Button
+          @click="toggleSidebar()"
+          class="bg-transparent hover:bg-gray-400"
+        >
+          <Icon name="mdi:show" size="20" color="white"></Icon>
+        </Button>
+      </div>
 
+      <div class="flex">
+        <Button @click="logout()" class="bg-transparent hover:bg-gray-400">
+          <Icon name="majesticons:logout" size="20" color="white"></Icon>
+        </Button>
+        <ColorModeToogle></ColorModeToogle>
+      </div>
+    </div>
     <div class="flex">
-      <Button @click="logout()" class="bg-transparent hover:bg-gray-400">
-        <Icon name="majesticons:logout" size="20" color="white"></Icon>
-      </Button>
-      <ColorModeToogle></ColorModeToogle>
+      <Icon name="ph:book-bold" size="20" @click="toggleSidebar()"></Icon>
+      <p class="text-white text-sm">
+        Repositories: {{ total_public_repositories }}
+      </p>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useAuthStore } from "@/stores/auth";
+import { useGithubStore } from "@/stores/github";
+import { storeToRefs } from "pinia";
 
 const emit = defineEmits(["toggleSidebar"]);
 function toggleSidebar() {
   emit("toggleSidebar");
 }
 
+const { total_public_repositories } = storeToRefs(useGithubStore());
+console.log(total_public_repositories.value);
 const { logUserOut } = useAuthStore();
 function logout() {
   logUserOut();
