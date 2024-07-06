bcac2b56ffafc59c61262a3c29742dc6c25cbccf
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-06 04:33:16 -0300

    Adiciona filtro de quantidade de itens por pagina para recuperar os repositorios

--- pages/index.vue
+++ pages/index.vue
@@ -17,10 +17,28 @@
               >
             </template>
             <template v-slot:message>
-              Use in case you have created a new repository and can"t find it
-              here
+              Use in case you have created a new repository and the total of
+              repositories here don"t reflect it
             </template>
           </LayoutHoverCard>
+          <div>
+            <label for="itemPerPagePagination"
+              >Choose the quantity per page:
+            </label>
+            <select
+              id="itemPerPagePagination"
+              v-model="selectedPaginationOption"
+              @change="handlePaginationChange"
+            >
+              <option
+                v-for="option in paginationOptions"
+                :key="option"
+                :value="option"
+              >
+                {{ option }}
+              </option>
+            </select>
+          </div>
         </div>
         <GithubRepositoryCard
           v-for="repository in response.repositories"
@@ -32,7 +50,7 @@
       <div class="flex justify-center items-center mt-4">
         <LayoutPagination
           :total="total"
-          :itemsPerPage="2"
+          :itemsPerPage="selectedPaginationOption"
           @update:page="handlePageUpdate"
           class="max-w-[80%]"
         ></LayoutPagination>
@@ -46,24 +64,45 @@ console.log(useCookie("token").value);
 import { useGithubStore } from "@/stores/github";
 import { storeToRefs } from "pinia";
 
+const selectedPaginationOption = ref(5);
+const paginationOptions = [5, 10, 15, 20, 25];
+async function handlePaginationChange() {
+  response.value = await getUserRepositories(
+    page.value,
+    +selectedPaginationOption.value
+  );
+}
+
 const { getUserRepositories } = useGithubStore();
 const { error } = storeToRefs(useGithubStore());
-
-const response = ref(await getUserRepositories(1, 2));
-const total = ref(response.value.total_pages_number * 2);
+const response = ref(
+  await getUserRepositories(1, selectedPaginationOption.value)
+);
+const total = ref(
+  response.value.total_pages_number * selectedPaginationOption.value
+);
 const page = ref(1);
-
 async function handlePageUpdate(newPage: number) {
   if (page.value == newPage) {
     return;
   }
   page.value = newPage;
-  response.value = await getUserRepositories(page.value, 2);
+  response.value = await getUserRepositories(
+    page.value,
+    selectedPaginationOption.value
+  );
+  total.value =
+    response.value.total_pages_number * selectedPaginationOption.value;
 }
 async function handleNoCachedGetAllRepositorysRequest() {
-  response.value = await getUserRepositories(page.value, 2, 0);
+  response.value = await getUserRepositories(
+    page.value,
+    selectedPaginationOption.value,
+    0
+  );
+  total.value =
+    response.value.total_pages_number * selectedPaginationOption.value;
 }
-//console.log(response.value.repositories);
 </script>
 
 <style scoped></style>
