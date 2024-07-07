369646c0436acf2b6d85c95739b08f7ca8aa2720
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-07 17:29:35 -0300

    Corrige layout e adiciona skeleton no carregamento dos dados

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <LayoutAlert v-if="error.message" :error="error" />
     <div v-else>
-      <div class="container flex flex-col gap-3">
+      <div class="flex flex-col gap-3">
         <div class="flex gap-3 flex-col items-center sm:flex-row mb-4">
           <Input
             type="text"
@@ -41,20 +41,32 @@
             </select>
           </div>
         </div>
-        <GithubRepositoryCard
-          v-for="repository in response.repositories"
-          :key="repository.id"
-          :repository="repository"
-          class="mb-2"
-        ></GithubRepositoryCard>
-      </div>
-      <div class="flex justify-center items-center mt-4">
-        <LayoutPagination
-          :total="total"
-          :itemsPerPage="selectedPaginationOption"
-          @update:page="handlePageUpdate"
-          class="max-w-[80%]"
-        ></LayoutPagination>
+
+        <div v-if="!loading" class="flex flex-col gap-3">
+          <GithubRepositoryCard
+            v-for="repository in response.repositories"
+            :key="repository.id"
+            :repository="repository"
+            class="mb-2"
+          ></GithubRepositoryCard>
+          <div class="flex justify-center items-center mt-4">
+            <LayoutPagination
+              :total="total"
+              :itemsPerPage="selectedPaginationOption"
+              :page="page"
+              @update:page="handlePageUpdate"
+              class="max-w-[80%]"
+            ></LayoutPagination>
+          </div>
+        </div>
+        <div v-else>
+          <div class="w-full">
+            <Skeleton
+              class="w-full h-[100px] mt-3"
+              v-for="quantity in selectedPaginationOption"
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -64,26 +76,30 @@
 console.log(useCookie("token").value);
 import { useGithubStore } from "@/stores/github";
 import { storeToRefs } from "pinia";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const selectedPaginationOption = ref(5);
 const paginationOptions = [5, 10, 15, 20, 25];
+const page = ref(1);
 async function handlePaginationChange() {
   response.value = await getUserRepositories(
     1,
     +selectedPaginationOption.value
   );
+  page.value = 1;
 }
 
 const { getUserRepositories } = useGithubStore();
-const { error } = storeToRefs(useGithubStore());
+const { error, loading } = storeToRefs(useGithubStore());
 const response = ref(
   await getUserRepositories(1, selectedPaginationOption.value)
 );
 const total = ref(
   response.value.total_pages_number * selectedPaginationOption.value
 );
-const page = ref(1);
+
 async function handlePageUpdate(newPage: number) {
+  console.log(newPage);
   if (page.value == newPage) {
     return;
   }
