924e3c6fc330c2869558d327a9a7b03e3d0c577f
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-07 23:19:36 -0300

    Retira conteudo do componente de repositorio

--- components/Github/Repository.vue
+++ components/Github/Repository.vue
@@ -1,29 +1,7 @@
 <template>
-  <div class="w-full">
-    <div class="border-2 border-blue-200 rounded-full w-full p-4">
-      <div class="flex gap-3 pb-1">
-        <h1 class="p-0 truncate">{{ repository.full_name }}</h1>
-        <p class="rounded-full bg-gray-300 px-3 text-sm text-black">
-          {{ repository.private ? "Private" : "Public" }}
-        </p>
-      </div>
-      <div class="flex items-center gap-3">
-        <div class="rounded-full w-2 h-2 bg-green-400"></div>
-        <Badge>{{ repository.language ?? "X" }}</Badge>
-        <p class="truncate">{{ repository.description }}</p>
-      </div>
-    </div>
-  </div>
+  <div class="w-full"></div>
 </template>
 
-<script setup lang="ts">
-import { Badge } from "@/components/ui/badge";
-const props = defineProps({
-  repository: {
-    type: Object,
-    required: true,
-  },
-});
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
