ad7d0bb10da52b06fdb9ba5ca69e66f9b4ab433e
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-08 17:59:13 -0300

    Corrige botão de login não ter o link por completo

--- pages/login.vue
+++ pages/login.vue
@@ -7,12 +7,12 @@
             class="flex flex-col gap-5 justify-center items-center min-w-[50%] min-h-[50%]"
           >
             <h1 class="text-2xl font-semibold text-black">Welcome back</h1>
-            <div class="bg-black p-5 rounded-lg flex gap-3">
-              <Icon name="mdi:github" color="white" size="24px"></Icon>
-              <NuxtLink to="api/auth/github" external class="text-white"
-                >Login with github</NuxtLink
-              >
-            </div>
+            <NuxtLink to="api/auth/github" external class="text-white">
+              <div class="bg-black p-5 rounded-lg flex gap-3">
+                <Icon name="mdi:github" color="white" size="24px"></Icon>
+                Login with github
+              </div>
+            </NuxtLink>
           </div>
         </CardContent>
       </Card>
