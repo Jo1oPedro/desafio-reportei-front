475bb5d383d724657e78242e7b1fa61874339c72
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-07 23:17:53 -0300

    Adiciona pagina de exibição do grafico dos commits

--- pages/repositories/[owner_name]/[repository_name]/[repository_id].vue
+++ pages/repositories/[owner_name]/[repository_name]/[repository_id].vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
+  <div class="container w-full">
     <!--<LayoutRepository :repository="repository"></LayoutRepository>-->
-    dale123
+    <!--<div v-for="repository in repositories" class="container">
+      <p class="text-white">{{ repository.url }}</p>
+    </div>-->
+    <LayoutAlert v-if="error.message" :error="error" />
+    <div class="min-h-[300px]" v-else>
+      <Line :data="chartData" :options="chartOptions" />
+    </div>
   </div>
 </template>
 
@@ -10,14 +16,64 @@ import { useGithubStore } from "@/stores/github";
 
 const { owner_name, repository_name, repository_id } = useRoute().params;
 
-const { getSpecificUserRepository } = useGithubStore();
+const { getSpecificUserRepository, getRepositoryCommits } = useGithubStore();
+const { error } = storeToRefs(useGithubStore());
 
-const repository = getSpecificUserRepository(
-  owner_name,
-  repository_name,
-  repository_id
+const repositories = ref(
+  await getSpecificUserRepository(owner_name, repository_name, repository_id)
 );
-console.log(repository);
+
+const commits = ref(
+  await getRepositoryCommits(owner_name, repository_name, repository_id)
+);
+
+const dates = [];
+const totalCommits = [];
+Object.keys(commits.value).forEach((key) => {
+  dates.push(formatDate(key));
+  totalCommits.push(commits.value[key]);
+});
+
+function formatDate(key) {
+  const str = key.toString();
+  return `${str.slice(0, 2)}-${str.slice(2, 4)}-${str.slice(4, 6)}`;
+}
+
+import { ref } from "vue";
+import { Line } from "vue-chartjs";
+
+const chartData = ref({
+  labels: dates,
+  datasets: [
+    {
+      label: "Commits",
+      backgroundColor: "rgb(255,255,224)",
+      data: totalCommits,
+      tension: 0.1,
+      borderColor: "rgb(123,240,0)",
+    },
+  ],
+});
+
+const chartOptions = ref({
+  responsive: true,
+  maintainAspectRatio: false,
+});
+
+function dale() {
+  chartData.value = {
+    labels: ["cascata", "dale"],
+    datasets: [
+      {
+        label: "Commits",
+        backgroundColor: "rgb(255,255,224)",
+        data: [3, 4],
+        tension: 0.1,
+        borderColor: "rgb(123,240,0)",
+      },
+    ],
+  };
+}
 </script>
 
 <style></style>
