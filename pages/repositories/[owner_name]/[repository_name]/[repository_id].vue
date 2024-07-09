1904257863011ddaf3bf61503a860c563b5cfc15
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-08 22:36:02 -0300

    Adiciona requisição sem cache para recuperar os commits e exibição do total de commits

--- pages/repositories/[owner_name]/[repository_name]/[repository_id].vue
+++ pages/repositories/[owner_name]/[repository_name]/[repository_id].vue
@@ -2,6 +2,20 @@
   <div class="container w-full">
     <LayoutAlert v-if="error.message" :error="error.message" />
     <div v-else>
+      <div class="flex justify-between mb-2">
+        <div>Total de commits: {{ totalCommitsNumber }}</div>
+        <LayoutHoverCard>
+          <template v-slot:trigger>
+            <Button @click="handleNoCachedCommitsRequest"
+              >Refresh commits</Button
+            >
+          </template>
+          <template v-slot:message>
+            Use in case you have new commits in the last 24hrs that are not
+            showing here
+          </template>
+        </LayoutHoverCard>
+      </div>
       <div v-if="loading">
         <Skeleton class="min-h-[300px]" />
       </div>
@@ -26,6 +40,7 @@ const { error, loading } = storeToRefs(useGithubStore());
 
 const repositories = ref(null);
 const commits = ref(null);
+const totalCommitsNumber = ref(0);
 
 onMounted(async () => {
   try {
@@ -35,26 +50,7 @@ onMounted(async () => {
       repository_name,
       repository_id
     );
-
-    const dates = [];
-    const totalCommits = [];
-    Object.keys(commits.value).forEach((key) => {
-      dates.push(formatDate(key));
-      totalCommits.push(commits.value[key]);
-    });
-
-    chartData.value = {
-      labels: dates,
-      datasets: [
-        {
-          label: "Commits",
-          backgroundColor: "rgb(255,255,224)",
-          data: totalCommits,
-          tension: 0.1,
-          borderColor: "rgb(123,240,0)",
-        },
-      ],
-    };
+    buildGraph(chartData, commits, totalCommitsNumber);
   } catch (e) {
     error.value = e.message;
   } finally {
@@ -65,6 +61,41 @@ onMounted(async () => {
 function formatDate(key) {
   const str = key.toString();
   return `${str.slice(0, 2)}-${str.slice(2, 4)}-${str.slice(4, 6)}`;
+}
+
+async function handleNoCachedCommitsRequest() {
+  commits.value = await getRepositoryCommits(
+    owner_name,
+    repository_name,
+    repository_id,
+    "no-cache"
+  );
+
+  buildGraph(chartData, commits, totalCommitsNumber);
+}
+
+function buildGraph(chartData, commits, totalCommitsNumber) {
+  const dates = [];
+  const totalCommits = [];
+  totalCommitsNumber.value = 0;
+
+  Object.keys(commits.value).forEach((key) => {
+    dates.push(formatDate(key));
+    totalCommits.push(commits.value[key]);
+    totalCommitsNumber.value += commits.value[key];
+  });
+  chartData.value = {
+    labels: dates,
+    datasets: [
+      {
+        label: "Commits",
+        backgroundColor: "rgb(255,255,224)",
+        data: totalCommits,
+        tension: 0.1,
+        borderColor: "rgb(123,240,0)",
+      },
+    ],
+  };
 }
 
 const chartData = ref({
