58f628d742c4440430f831f0b6b454df28fd0050
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-09 04:36:53 -0300

    Corrige url de login

--- server/api/auth/github.get.ts
+++ server/api/auth/github.get.ts
@@ -6,14 +6,18 @@ export default oauth.githubEventHandler({
   },
   async onSuccess(event, { user, tokens }) {
     try {
-      const response = await axios.post("http://localhost:8888/api/register", {
-        name: user.name,
-        email: user.email ?? "sememail@gmail.com",
-        github_id: user.id.toString(),
-        avatar_url: user.avatar_url,
-        github_login: user.login,
-        access_token: tokens.access_token,
-      });
+      const runtimeConfig = useRuntimeConfig();
+      const response = await axios.post(
+        `${runtimeConfig.public.url}/api/register`,
+        {
+          name: user.name,
+          email: user.email ?? "sememail@gmail.com",
+          github_id: user.id.toString(),
+          avatar_url: user.avatar_url,
+          github_login: user.login,
+          access_token: tokens.access_token,
+        }
+      );
       setCookie(event, "token", response.data.token);
       setCookie(event, "authenticatedUser", JSON.stringify(response.data.user));
       return sendRedirect(event, "/");
